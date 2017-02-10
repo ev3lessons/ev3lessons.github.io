67b2baa3f67bd9a11083af97a9ef2a35b93e6f78
Sanjay Seshan <sanjay.seshan@outlook.com>
2017-02-10 13:58:37 -0500

    Updates

--- translations/en-us/mission.js
+++ translations/en-us/mission.js
@@ -21,16 +21,16 @@ document.write('\
 		                    </div>\
 		                    <div class="post_content group  no-skills ">\
 \
-				      <p> This site was founded by Sanjay and Arvind Seshan from First Lego League (FLL) Team Not the Droids You Are Looking For in Pittsburgh, Pennsylvania, USA in 2014. You can read more about them on the Contributors Page. The tutorials and resources on this site are intended for students in classrooms learning robotics, robotics clubs, scouts, and robotics teams.</p>\
+				      <p> This site was founded by Sanjay and Arvind Seshan from First Lego League (FLL) Team Not the Droids You Are Looking For in Pittsburgh, Pennsylvania, USA in 2014. You can read more about them on the Contributors Page. The tutorials and resources on this site are intended for all students in classrooms learning robotics, robotics clubs, scouts, and robotics teams.</p>\
 \
 				      <p>Our primary mission is to provide all robotics students with a good starting point for coding well. If you start right, you can learn more and do more. We think learning-by-doing is important and so we have included challenges at the end of each lesson. We also provide a complete set of lessons, from Beginner through Advanced, in a format that is concise and easy-to-use.</p>\
 \
 				     \
 \
-				      <p>A second mission is related to our tag line "Let"s Learn Together". Our goal is to create an environment where EV3 users (FLL teams or individuals) can share their designs, code and ideas. As a result, EV3Lessons has a growing list of international collaborators from the Netherlands, Brazil, Canada, Mexico and the United States. For First Lego League teams, this is a website where the idea of Gracious Professionalism and Coopertition can be put into practice every day. Anyone who would like to make a contribution to the site is welcome.</p>\
+				      <p>A second mission is related to our tag line "Let's Learn Together". Our goal is to create an environment where EV3 users (FLL teams or individuals) can share their designs, code and ideas. As a result, EV3Lessons has a growing list of international collaborators from the Netherlands, Brazil, Canada, Mexico and the United States. For First Lego League teams, this is a website where the idea of Gracious Professionalism and Coopertition can be put into practice every day. Anyone who would like to make a contribution to the site is welcome.</p>\
 \
 \
-				       <p>If you are an FLL team, your season will be successful because of the work you put into it. We do not provide solutions to any First Lego League missions. We want students to take the lessons on this site, learn from them, adapt them, add to them, and then solve your challenges by combining concepts and ideas.</p>\
+				       <p>If you are a robotics team, your season will be successful because of the work you put into it. We do not provide solutions to any First Lego League or World Robot Olympiad missions. We want students to take the lessons on this site, learn from them, adapt them, add to them, and then solve your challenges by combining concepts and ideas. We provide only the building blocks for you to build upon.</p>\
 \
 				\
 				      <p>It is okay to:\
