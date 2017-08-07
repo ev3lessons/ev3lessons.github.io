d5366321a2956c55eb6135150903dc94e0fa19e7
Sanjay Seshan <sanjay.seshan@outlook.com>
2017-08-07 13:12:00 -0400

    translation url style v3.0--> v2.5 deprecated

--- _site/topbar.js
+++ _site/topbar.js
@@ -1,9 +1,19 @@
-function get_browser_version(){
+function get_browser_version(){	
   var N=navigator.appName, ua=navigator.userAgent, tem;
   var M=ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
   if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
   M=M? [M[1], M[2]]: [N, navigator.appVersion, '-?'];
   return M[1];
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 var browser = navigator.appName;
@@ -14,29 +24,29 @@ if (browser=="Microsoft Internet Explorer") {
         document.location.href="http://classic.ev3lessons.com/" 
 } 
 
-
+/*
 if (window.location.hash) {
     // Puts hash in variable, and removes the # character
     var language = window.location.hash.substring(1);
-if (window.location.hash == "#br") {
-  window.location.hash = "#pt-br";
-  setTimeout(function(){location.reload(true);},100);
-}
+//if (window.location.hash == "#br") {
+//  window.location.hash = "#pt-br";
+//  setTimeout(function(){location.reload(true);},100);
+//}
 } else {
     // window.location.replace("#en-us");
     var language = "en-us";
     // location.reload();
 }
+*/
 
-
-if (window.location.hash != ""){
-document.cookie = "ev3cookie"+window.location.hash.substring(1);
-
-if (document.cookie != ""){
-//deleteAllCookies()
+if (getParameterByName('lang') != null) {
+    var language = getParameterByName('lang');
+localStorage.lang = "ev3cookie"+language;
+} else {
+var language = 'en-us'
 }
-document.cookie = "ev3cookie"+window.location.hash.substring(1);
-}
+
+
 
 
 function deleteAllCookies() {
@@ -52,24 +62,38 @@ document.cookie=""
 //alert(document.cookie)
 langs=['en-us:United States:English','es:Spain:Español','nl:Netherlands:Nederlands','ar:Saudi Arabia:العَرَبِيةُ‎‎','pt-br:Brazil:Portugues','el:Greece:Ελληνικά','fr:France:français','ct:../ct:català','he:Israel:עברית','de:Germany:Deutsche']
 	
-if (document.cookie && window.location.hash == "") {
-str =document.cookie
+//if (document.cookie && window.location.hash == "") {
+//str =document.cookie
+
+str =localStorage.lang
+if (localStorage.lang && window.location.hash == "") {
 var i;
 for (i = 0; i < langs.length; i++) {
     tmp =str.search('ev3cookie'+langs[i].split(':')[0])
     if (tmp != -1) {
-window.location.hash = "#"+langs[i].split(':')[0]
+//window.location.hash = "#"+langs[i].split(':')[0]
 //  setTimeout(function(){location.reload(true);},100);
     var language = langs[i].split(':')[0];
 
 }
 }
 
+
+
 //alert('a')
 //window.location.hash = "#"+document.cookie.split(';')[0]
 //  setTimeout(function(){location.reload(true);},100);
 }
-var page = window.location.href.split("#")[0] + '#';
+
+
+
+
+function createCookie(lang){
+localStorage.lang = "ev3cookie"+lang
+}
+
+
+var page = window.location.href.split("?")[0].split("#")[0] + '?lang=';
 var i;
 for (i = 0; i < langs.length; i++) {
     window[langs[i].split(':')[0].split('-')[0]] = page + langs[i].split(':')[0];
@@ -91,6 +115,23 @@ var de  = ''+ page +'#de';
 // var href = window.location.href;
 // alert(href);
 
+
+if (window.location.hash) {
+    // Puts hash in variable, and removes the # character
+    var language = window.location.hash.substring(1);
+//if (window.location.hash == "#br") {
+//  window.location.hash = "#pt-br";
+//  setTimeout(function(){location.reload(true);},100);
+//}
+} 
+/*else {
+    // window.location.replace("#en-us");
+    var language = "en-us";
+    // location.reload();
+}*/
+
+
+
 document.write('\
 <!--  <link rel="stylesheet" type="text/css" media="all" href="stylenotable.css" />-->\
         <div id="topbar">\
@@ -103,10 +144,10 @@ document.write('\
 ')
 var i;
 for (i = 0; i < langs.length; i++) {
-    window[langs[i].split(':')[0].split('-')[0]] = ''+ page +langs[i].split(':')[0];
+//    window[langs[i].split(':')[0].split('-')[0]] = ''+ page +langs[i].split(':')[0];
 document.write('\
 		<div class="socials-default-small  default ovd">\
-		  <a href="'+ window[langs[i].split(":")[0].split("-")[0]] +'" onclick="document.cookie=\'ev3cookie'+langs[i].split(':')[0]+'\';deleteAllCookies();document.cookie=\'ev3cookie'+langs[i].split(':')[0]+'\';setTimeout(function(){location.reload(true);},100)">\
+		  <a href="'+ window[langs[i].split(":")[0].split("-")[0]] +'" onclick="createCookie(\''+langs[i].split(":")[0]+'\');setTimeout(function(){location.reload(true);},100)">\
 		  <img src="images/icons/countries/small/'+langs[i].split(":")[1]+'.png" alt="'+langs[i].split(":")[2]+'" title="'+langs[i].split(":")[2]+'" width="26" height="26" border="0">\
 		  </a>\
 		</div>\
