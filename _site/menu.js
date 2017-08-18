95d3c4dbfbfb1b060e9b3166ab2a40faeabf567e
Sanjay Seshan <sanjay.seshan@outlook.com>
2017-08-18 11:47:15 -0400

    simplified translations filter

--- _site/menu.js
+++
@@ -1,4 +0,0 @@
-document.write('<div id="includedContent"></div>')
-    $(function(){
-      $("#includedContent").load("translations/"+language+"/menu.html"); 
-    })
--- _site/translations/ar/ar.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-$scope.language= window.location.hash.substr(1);
-$scope.Introduction= 'Introductionaaa';
-
-});
